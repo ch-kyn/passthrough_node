c75d30afa01203112ad8879284fdc788518ac750
ch <chriskng@stud.ntnu.no>
2025-02-19 12:47:19 +0100

    hii

--- passthrough.py
+++ passthrough.py
@@ -3,13 +3,16 @@ import requests
 
 app = FastAPI()
 
-MAIN_NODE_URL = "https://cloud-lab5.onrender.com"  # Your actual main node URL
+MAIN_NODE_URL = "https://cloud-lab5.onrender.com"
 
-@app.get("/{path_parameter}")
-def passthrough_node_get(path_parameter: str):
-    get_url = f"{MAIN_NODE_URL}/{path_parameter}"
+@app.get("/{full_path:path}")
+def passthrough_node_get(full_path: str):
+    get_url = f"{MAIN_NODE_URL}/{full_path}"
     try:
         response = requests.get(get_url)
         return response.json()
     except requests.exceptions.RequestException as e:
         return {"error": "Failed to fetch data", "details": str(e)}
+    
+    # add cache here
+    
